e19f8b6ecfa380f4f946389fccbf67cba5f98cb4
Maykon Lacerda <maykonalvslacerda@gmail.com>
2021-07-23 12:12:20 -0300

    fix

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,7 @@
 module.exports = {
     type: 'postgres',
     url: process.env.DATABASE_URL,
+    ssl: true,
     migrations: ['./src/database/migrations/*.ts'],
     entities: ['./src/modules/**/entities/*.ts'],
     cli: {
