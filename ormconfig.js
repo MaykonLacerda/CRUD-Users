d11631c167eade177cd6ec2f997c2f921bb1126c
Maykon Lacerda <maykonalvslacerda@gmail.com>
2021-07-24 19:25:42 -0300

    Branch to development

--- ormconfig.js
+++ ormconfig.js
@@ -1,12 +1,9 @@
 module.exports = {
     type: 'postgres',
     url: process.env.DATABASE_URL,
-    ssl: true,
-    extra: { ssl: { rejectUnauthorized: false } },
     migrations: ['./dist/database/migrations/*.js'],
     entities: ['./dist/modules/**/entities/*.js'],
     cli: {
       migrationsDir: './dist/database/migrations'
     }
   };
-  //sa
