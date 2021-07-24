d4eee1946c71f4b274ae82e330beda6d90659a8b
Maykon Lacerda <maykonalvslacerda@gmail.com>
2021-07-24 19:09:21 -0300

    fix: to deploy

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,8 @@
 module.exports = {
     type: 'postgres',
     url: process.env.DATABASE_URL,
-    ssl: true,
-    extra: { ssl: { rejectUnauthorized: false } },
+    // ssl: true,
+    // extra: { ssl: { rejectUnauthorized: false } },
     migrations: ['./dist/database/migrations/*.js'],
     entities: ['./dist/modules/**/entities/*.js'],
     cli: {
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
+import dotenv from 'dotenv'
+
+dotenv.config();
+
 import { app } from './app';
 import dayjs from 'dayjs';
 import advancedFormat from 'dayjs/plugin/advancedFormat';
 import weekOfYear from 'dayjs/plugin/weekOfYear';
+
 
 dayjs.extend(weekOfYear);
 dayjs.extend(advancedFormat);
