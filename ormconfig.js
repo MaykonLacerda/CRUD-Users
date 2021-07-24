6d41319c469b8c8878649b04ff790c310eb4a597
Maykon Lacerda <maykonalvslacerda@gmail.com>
2021-07-24 19:20:22 -0300

    fix: to deploy

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,12 @@
 module.exports = {
     type: 'postgres',
     url: process.env.DATABASE_URL,
-    // ssl: true,
-    // extra: { ssl: { rejectUnauthorized: false } },
+    ssl: true,
+    extra: { ssl: { rejectUnauthorized: false } },
     migrations: ['./dist/database/migrations/*.js'],
     entities: ['./dist/modules/**/entities/*.js'],
     cli: {
       migrationsDir: './dist/database/migrations'
     }
   };
+  //sa
