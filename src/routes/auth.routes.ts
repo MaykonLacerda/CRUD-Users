b38bfca12fe8137943907573dc54d81a8cb73b9e
Maykon Lacerda <maykonalvslacerda@gmail.com>
2021-07-26 11:49:40 -0300

    fix: remove refresh token controller & service

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,13 +1,10 @@
 import { Router } from 'express';
 import { AuthenticateUserController } from '../modules/accounts/useCases/authenticateUser/AuthenticateUserController';
-import { RefreshTokenController } from '../modules/accounts/useCases/refreshToken/RefreshTokenController';
 
 const authRoutes = Router();
 
 const authenticateController = new AuthenticateUserController();
-const refreshTokenController = new RefreshTokenController();
 
 authRoutes.post('/sessions', authenticateController.handle);
-authRoutes.post('/refresh-token', refreshTokenController.handle);
 
 export { authRoutes };
